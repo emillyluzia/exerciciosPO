d04fd4a3852e7ee3fc8b8d046a77f712ff932ffe
emillyluzia <emillyluzia416@gmail.com>
2023-08-24 14:00:05 -0300

    atualizando conta bancaria

--- ContaBancaria.js
+++ ContaBancaria.js
@@ -23,8 +23,18 @@ class ContaBancaria {
         }
     }
 }
-let minhaconta = new ContaBancaria("João", 1000);
+let minhaconta = new ContaBancaria("Emilly", 40000);
 console.log(minhaconta.consultarSaldotitular());
 console.log(minhaconta.depositar(500));
 console.log(minhaconta.sacar(200));
-console.log(minhaconta.sacar(1500));
+console.log(minhaconta.sacar(55000));
+let minhaconta2 = new ContaBancaria("Guilherme", 30000);
+console.log(minhaconta2.consultarSaldotitular());
+console.log(minhaconta2.depositar(500));
+console.log(minhaconta2.sacar(200));
+console.log(minhaconta2.sacar(35000));
+let minhaconta3 = new ContaBancaria("Gabriela", 10000);
+console.log(minhaconta3.consultarSaldotitular());
+console.log(minhaconta3.depositar(500));
+console.log(minhaconta3.sacar(200));
+console.log(minhaconta3.sacar(15000));
